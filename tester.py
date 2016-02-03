80f3e166ddc22101f20b1840a6eec93b0652ae58
Algo-FX <kevin.williams@algo-fx.net>
2016-02-03 14:34:43 -0500

    Deleteing old outdated files that are not needed

--- tester.py
+++
@@ -1,9 +0,0 @@
-
-
-import cufflinks as cf
-import pandas as pd
-
-cf.set_config_file(world_readable=True,offline=False)
-
-cf.datagen.lines(1,200).ta_plot(study='rsi',periods=14,title='Relative Strength Index')
-
