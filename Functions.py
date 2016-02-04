c4d90a8017707423b6f30abdaad14a7cc91b1299
Algo-FX <kevin.williams@algo-fx.net>
2016-02-04 15:51:34 -0500

    Revised datapath again to point to data/ folder in the working directory .gitignore now exludes the folder so it does not need to be external to the project - Also added the Beep function, but does not seem to work when imported, not sure why (may need to set up as a true module)

--- Functions.py
+++ Functions.py
@@ -1,12 +1,17 @@
+import sys
 import datetime as dt
+import winsound
 
 #-----------------------------------------------------------------
 # Common Variables
 #-----------------------------------------------------------------
-datapath = "../Donchian_Channel_data/"
+datapath = "data/"
 
 #-----------------------------------------------------------------
 # function to convert 'DateTime' to seconds
 #-----------------------------------------------------------------
 def seconds(time):
     return (time - dt.datetime(1970, 1, 1)).total_seconds()
+
+def beep(sound):
+    winsound.PlaySound('%s.wav' % sound, winsound.SND_FILENAME)
